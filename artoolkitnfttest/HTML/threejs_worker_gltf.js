e5d241c04ea8081204049d498167136f2e8ef99a
sachopu <kpadsebyz@gmail.com>
2020-03-01 05:20:50 +0900

    Update threejs_worker_gltf.js

--- artoolkitnfttest/HTML/threejs_worker_gltf.js
+++ artoolkitnfttest/HTML/threejs_worker_gltf.js
@@ -6,7 +6,7 @@ function isMobile() {
     return /Android|mobile|iPad|iPhone/i.test(navigator.userAgent);
 }
 
-var interpolationFactor = 2;
+var interpolationFactor = 1;
 
 var trackedMatrix = {
     // for interpolation
